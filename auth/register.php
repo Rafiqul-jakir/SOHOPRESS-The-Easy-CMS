c81f5bf6f8eee13fce0b6611121ef5dcae49a15d
Rafiqul Islam <rafiquljakir@gmail.com>
2024-02-06 18:00:34 +0600

    complete users Registration page

--- auth/register.php
+++ auth/register.php
@@ -1,4 +1,33 @@
-<?php require "../includes/header.php" ?>
+<?php 
+  require "../includes/header.php";
+  require "../config/config.php";  
+?>
+
+
+
+<?php
+
+    if(isset($_POST['submit'])){
+      if(empty($_POST['email']) || empty($_POST['username']) || empty($_POST['password'])){
+        echo "<script> alert('all field are required')</script>";
+      }else{
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $insert = $conn->prepare("INSERT INTO users(email, username, password) VALUES(:email, :username, :password)");
+        $insert->execute([
+          ":email" => $email,
+          ":username" => $username,
+          ":password" => $password,
+        ]);
+        header("location: login.php");
+      }
+    }
+
+
+
+?>
        
                 <!-- Main Content-->
         <div class="container px-4 px-lg-5">
@@ -6,18 +35,18 @@
             <form method="POST" action="register.php">
               <!-- Email input -->
               <div class="form-outline mb-4">
-                <input type="email" name="email" id="form2Example1" class="form-control" placeholder="Email" />
+                <input type="email" name="email" id="form2Example1" class="form-control" placeholder="Email" required />
                
               </div>
 
               <div class="form-outline mb-4">
-                <input type="" name="username" id="form2Example1" class="form-control" placeholder="Username" />
+                <input type="" name="username" id="form2Example1" class="form-control" placeholder="Username" required />
                
               </div>
 
               <!-- Password input -->
               <div class="form-outline mb-4">
-                <input type="password" name="password" id="form2Example2" placeholder="Password" class="form-control" />
+                <input type="password" name="password" id="form2Example2" placeholder="Password" class="form-control" required />
                 
               </div>
 
--- config/config.php
+++ config/config.php
@@ -1,22 +1,18 @@
 <?php
 
-    try {
+// Establish database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sohopress";
 
-        $host = "localhost";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
-        //Database
-        $db_name = "sohopress";
-    
-        //user
-        $user = "root";
-    
-        //password
-        $pass = "";
-    
-        $conn = new PDO("mysql:host = $host; dbname = $db_name", $user, $pass);
-        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-    }
 
 ?>
