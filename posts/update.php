fac9a1286878ea997b8a27f66f21f6496639e7c5
Rafiqul Islam <rafiquljakir@gmail.com>
2024-02-10 12:53:27 +0600

    Update Post With Image

--- posts/update.php
+++ posts/update.php
@@ -11,16 +11,29 @@
         $p_post->execute();
         $fetch = $p_post->fetch(PDO::FETCH_OBJ);
 
+
         //update query
         if(isset($_POST['submit'])){
+          
+            unlink("../assets/img/post_images/".$fetch->post_image."");
             $title = $_POST['title'];
             $sub_title = $_POST['sub_title'];
             $description = $_POST['description'];
 
-            $update = $conn->query("UPDATE posts SET title='$title',sub_title='$sub_title',description='$description' WHERE ID = '$update_id'");
+            $image = $_FILES['post_img']['name'];  
+            $image_tmp = $_FILES['post_img']['tmp_name']; 
+    
+            $user_name = $_SESSION['user_name'];
+    
+            $dir = "../assets/img/post_images/".basename($image);
+
+            $update = $conn->query("UPDATE posts SET title='$title',sub_title='$sub_title',description='$description', post_image = '$image' WHERE ID = '$update_id'");
             $update->execute();
 
-            header("location: http://localhost/SOHOPRESS/posts/post.php?post_id=".$update_id."");
+            if(move_uploaded_file($image_tmp, $dir)){
+              header("location: http://localhost/SOHOPRESS/posts/post.php?post_id=".$update_id."");
+          }
+            
         }
     }
 
@@ -45,14 +58,15 @@
                 <textarea type="text" name="description" id="form2Example1" class="form-control" placeholder="<?php echo $fetch->description ?>" rows="8" required></textarea>
             </div>
 
-              
+            <?php echo "<img src='../assets/img/post_images/{$fetch->post_image}' width='100px' height='100px'/>"; ?>
+
              <div class="form-outline mb-4">
                 <input type="file" name="post_img" id="form2Example1" class="form-control" placeholder="image" />
             </div>
 
 
               <!-- Submit button -->
-              <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">create</button>
+              <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">Update</button>
 
           
             </form>
