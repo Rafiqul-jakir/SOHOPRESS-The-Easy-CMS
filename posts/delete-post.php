fa83ad23cace84a305f8a502b1add5a26806a802
Rafiqul Islam <rafiquljakir@gmail.com>
2024-02-10 12:26:09 +0600

    delete Post With Image

--- posts/delete-post.php
+++ posts/delete-post.php
@@ -4,6 +4,12 @@
 
     if(isset($_GET['post_id'])){
         $post_id = $_GET['post_id'];
+
+        $select_img = $conn->query("SELECT * FROM posts WHERE ID = '$post_id'");
+        $select_img->execute();
+        $img_name = $select_img->fetch(PDO::FETCH_OBJ);
+        unlink("../assets/img/post_images/".$img_name->post_image."");
+
         $delete_post = $conn->query("DELETE FROM `posts` WHERE ID = '$post_id'");
         $delete_post->execute();
 
