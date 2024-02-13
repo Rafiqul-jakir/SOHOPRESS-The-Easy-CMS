ff03c03353bdfeb789287d9bb680ce65c5993ead
Rafiqul Islam <rafiquljakir@gmail.com>
2024-02-14 00:36:06 +0600

    Showing the all admins Data

---
+++ admin-panel/admins/admins.php
@@ -0,0 +1,54 @@
+<?php
+  require "../layouts/header.php";
+  require "../../config/config.php";
+
+
+  if(!isset($_SESSION['admin_email'])){
+    header("location: ".ADMINURL."");
+  }
+
+  $admin_query = $conn->query("SELECT * FROM admins");
+  $admin_query->execute();
+  $all_admin = $admin_query->fetchAll(PDO::FETCH_OBJ);
+
+?>
+    <div class="container-fluid">
+
+          <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-4 d-inline">Admins</h5>
+             <a  href="create-admins.html" class="btn btn-primary mb-4 text-center float-right">Create Admins</a>
+              <table class="table">
+                <thead>
+                  <tr class="text-center">
+                    <th scope="col">#</th>
+                    <th scope="col">username</th>
+                    <th scope="col">email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach($all_admin as $all_admin): ?>
+                  <tr class="text-center">
+                    <th scope="row"><?php echo $all_admin->ID ?></th>
+                    <td><?php echo $all_admin->admin_name ?></td>
+                    <td><?php echo $all_admin->admin_email ?></td>
+                   
+                  </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table> 
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+  </div>
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
