15b979ab0580057f97fdab626523fc948e1ed4d6
Rafiqul Islam <rafiquljakir@gmail.com>
2024-02-17 14:06:47 +0600

    Comment add for admin review

--- posts/post.php
+++ posts/post.php
@@ -15,16 +15,21 @@
         exit;
     }
 
-    $cmt_query = $conn->query("SELECT * FROM comments WHERE post_id = '$post_id'");
+    $cmt_query = $conn->query("SELECT * FROM comments WHERE post_id = '$post_id' AND status = '1' ORDER BY created_at DESC");
     $cmt_query->execute();
     $all_cmt = $cmt_query->fetchAll(PDO::FETCH_OBJ);
+
+    $cmt_check_query = $conn->query("SELECT * FROM comments WHERE post_id = '$post_id' AND status = '0' AND user_name = '$_SESSION[user_name]'");
+    $cmt_check_query->execute();
+
     if(isset($_POST['submit']) && isset($_GET['post_id'])){
         $post_id = $_GET['post_id'];
-        $insert_cmt = $conn->prepare("INSERT INTO comments(comment, post_id, user_name) VALUES(:comment, :post_id, :user_name)");
+        $insert_cmt = $conn->prepare("INSERT INTO comments(comment, post_id, user_name, status) VALUES(:comment, :post_id, :user_name, :status)");
         $insert_cmt->execute([
             ":comment" => $_POST['comment'],
             ":post_id" => $post_id,
             ":user_name" =>$_SESSION['user_name'],
+            ":status" => 0,
         ]);
         header("location: http://localhost/SOHOPRESS/posts/post.php?post_id=".$post_id ."");
     }
@@ -85,26 +90,36 @@
 
                 <div class="card">
                     <div class="card-body">
-                        <?php foreach($all_cmt as $all_cmt): ?>
-                            <div class="d-flex flex-start align-items-center">
-                                <?php
-                                    $input_date = $all_cmt->created_at;
-                                    $output_date = date("F j, Y", strtotime($input_date));
-                                ?>
-                                <div>
-                                    <h6 class="fw-bold text-primary">
-                                        <?php echo $all_cmt->user_name; ?>
-                                        <small class="p-3 text-black">(<?php echo $output_date ?>)</small>
-                                    </h6>
+                        <?php if($cmt_query->rowCount() > 0 ): ?>
+                            <?php foreach($all_cmt as $all_cmt): ?>
+                                <div class="d-flex flex-start align-items-center">
+                                    <?php
+                                        $input_date = $all_cmt->created_at;
+                                        $output_date = date("F j, Y", strtotime($input_date));
+                                    ?>
+                                    <div>
+                                        <h6 class="fw-bold text-primary">
+                                            <?php echo $all_cmt->user_name; ?>
+                                            <small class="p-3 text-black">(<?php echo $output_date ?>)</small>
+                                        </h6>
+                                    </div>
                                 </div>
+                                <div>
+                                    <p class="mt-3 mb-4 pb-2 pl-5">
+                                        <?php echo $all_cmt->comment; ?>
+                                    </p>
+                                    <hr class="my-4" />
+                                </div>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <div class="text-center">
+                                <p>No comment for this Post . <br> Be the first Comment !!</p>
                             </div>
-                            <div>
-                                <p class="mt-3 mb-4 pb-2 pl-5">
-                                    <?php echo $all_cmt->comment; ?>
-                                </p>
-                                <hr class="my-4" />
-                            </div>
-                        <?php endforeach; ?>
+                        <?php endif; ?>
+
+                        <?php if($cmt_check_query->rowCount() > 0): ?>
+                            <div class="text-center alert alert-success"> Your Comment is forward to the admin for Approval !!</div>
+                        <?php endif; ?>
                     </div>
                     
                   <?php if(isset($_SESSION['user_name'])): ?>
@@ -116,7 +131,7 @@
                                 
                                     <div class="form-outline w-100">
                                         <textarea class="form-control" id="" placeholder="write message" rows="4"
-                                        name="comment"></textarea>
+                                        name="comment" required></textarea>
                                     
                                     </div>
                                 </div>
